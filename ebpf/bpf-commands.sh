615ec318305d00c1a1062798724fb50c76be11c2
Haridas N <haridas.nss@gmail.com>
2020-09-29 15:38:46 +0530

    bpf commands

---
+++ ebpf/bpf-commands.sh
@@ -0,0 +1,6 @@
+
+sudo opensnoop-bpfcc
+
+# List the laoded pbf programs.
+sudo bpftool prog list
+sudo bpftool prog dump xlated id 3
---
+++ ebpf/bpf-setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get install unzip
+sudo apt-get install bpfcc-tools
+sudo apt-get install build-essential
+sudo apt-get install libelf-dev
+sudo apt-get install zlib1g-dev
